19c17b924fe85122c849646f09ceef15d3976fef
dyomanik <93871743+dyomanik@users.noreply.github.com>
2022-09-02 17:04:55 +0500

    Added Lesson2 Assets

--- Assets/Scripts/Core/MainBuilding.cs
+++ Assets/Scripts/Core/MainBuilding.cs
@@ -1,28 +1,24 @@
 using Abstractions;
+using Abstractions.Commands;
+using Abstractions.Commands.CommandsInterfaces;
 using UnityEngine;
 
-namespace Core
+public sealed class MainBuilding : CommandExecutorBase<IProduceUnitCommand>, ISelectable
 {
-    public sealed class MainBuilding : MonoBehaviour, IUnitProducer, ISelectable
-    {
-        public float Health => _health;
-        public float MaxHealth => _maxHealth;
-        public Sprite Icon => _icon;
+    public float Health => _health;
+    public float MaxHealth => _maxHealth;
+    public Sprite Icon => _icon;
 
-        [SerializeField] private GameObject _unitPrefab;
-        [SerializeField] private Transform _unitsParent;
+    [SerializeField] private Transform _unitsParent;
 
-        [SerializeField] private float _maxHealth = 1000;
-        [SerializeField] private Sprite _icon;
+    [SerializeField] private float _maxHealth = 1000;
+    [SerializeField] private Sprite _icon;
 
-        private float _health = 1000;
+    private float _health = 1000;
 
-        public void ProduceUnit()
-        {
-            Instantiate(_unitPrefab,
-                new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10)),
-                Quaternion.identity,
-                _unitsParent);
-        }
-    }
+    public override void ExecuteSpecificCommand(IProduceUnitCommand command) 
+        => Instantiate(command.UnitPrefab, 
+            new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10)), 
+            Quaternion.identity, 
+            _unitsParent);
 }
