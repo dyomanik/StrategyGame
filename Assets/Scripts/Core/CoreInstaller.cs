f4501654a2189ac10afbcedccc94561a72004143
dyomanik <93871743+dyomanik@users.noreply.github.com>
2022-09-28 20:21:47 +0500

    Added Lesson7 homework

--- Assets/Scripts/Core/CoreInstaller.cs
+++ Assets/Scripts/Core/CoreInstaller.cs
@@ -1,12 +1,17 @@
-﻿using Zenject;
+﻿using Abstractions;
+using Zenject;
+using UnityEngine;
 
 namespace Core
 {
     public class CoreInstaller : MonoInstaller
     {
+        [SerializeField] private GameStatus _gameStatus;
+
         public override void InstallBindings()
         {
             Container.BindInterfacesAndSelfTo<TimeModel>().AsSingle();
+            Container.Bind<IGameStatus>().FromInstance(_gameStatus);
         }
     }
 }
--- Assets/Scripts/Core/FractionMember.cs
+++ Assets/Scripts/Core/FractionMember.cs
@@ -1,12 +1,80 @@
 using Abstractions;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Core
 {
     public class FractionMember : MonoBehaviour, IFractionMember
     {
-        public int FractionId => _fractionId;
         [SerializeField] private int _fractionId;
+
+        private static Dictionary<int, List<int>> _membersCount = 
+            new Dictionary<int, List<int>>();
+
+        public int FractionId => _fractionId;
+
+        public static int FractionsCount
+        {
+            get
+            {
+                lock (_membersCount)
+                {
+                    return _membersCount.Count;
+                }
+            }
+        }
+
+        public static int GetWinner()
+        {
+            lock (_membersCount)
+            {
+                return _membersCount.Keys.First();
+            }
+        }
+
+        private void Awake()
+        {
+            if (_fractionId != 0)
+            {
+                Register();
+            }
+        }
+
+        private void OnDestroy()
+        {
+            Unregister();
+        }
+
+        private void Register()
+        {
+            lock (_membersCount)
+            {
+                if (!_membersCount.ContainsKey(_fractionId))
+                {
+                    _membersCount.Add(_fractionId, new List<int>());
+                }
+                if (!_membersCount[_fractionId].Contains(GetInstanceID()))
+                {
+                    _membersCount[_fractionId].Add(GetInstanceID());
+                }
+            }
+        }
+
+        private void Unregister()
+        {
+            lock (_membersCount)
+            {
+                if (_membersCount[_fractionId].Contains(GetInstanceID()))
+                {
+                    _membersCount[_fractionId].Remove(GetInstanceID());
+                }
+                if (_membersCount[_fractionId].Count == 0)
+                {
+                    _membersCount.Remove(_fractionId);
+                }
+            }
+        }
 
         public void SetFraction(int fractionId)
         {
